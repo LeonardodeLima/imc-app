56e20fbd9a59e743d11881a1c5d7c7987fb6a1ad
leonardo lima <leonardo.flatpb@gmail.com>
2018-03-04 17:48:56 -0300

    Adicionado notificação do tipo de IMC com animação de fundo da tela

--- app/src/main/java/com/pos/leonardo/imc_app/MainActivity.java
+++ app/src/main/java/com/pos/leonardo/imc_app/MainActivity.java
@@ -1,5 +1,11 @@
 package com.pos.leonardo.imc_app;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ConstraintLayout  _telaPrincpal = (ConstraintLayout)findViewById(R.id.telaPrincpal);
 
         final EditText _peso    = (EditText) findViewById(R.id.peso);
         final EditText _altura  = (EditText) findViewById(R.id.altura);
@@ -49,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         btok.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("NewApi")
             @Override
             public void onClick(View view) {
                 if(!_input_nome.getText().toString().trim().isEmpty()){
@@ -56,12 +65,56 @@ public class MainActivity extends AppCompatActivity {
                     Double imc =  peso/Math.pow(altura, 2);
                     dialog.setVisibility(view.GONE);
 
-                    _textimc.setText("Olá "+_input_nome.getText().toString()+" SEU IMC É: "+String.valueOf(imc));
+                     String[] notificacao = checkImc(imc);
+
+                     _textimc.setText("Maoiê "+_input_nome.getText().toString()+"! SEU IMC É: "+String.valueOf(Math.round(imc *100.0)/100.0)
+                                                                                +"\n"+notificacao[0]);
+                    _telaPrincpal.setBackgroundColor(Color.parseColor(String.valueOf(notificacao[1])));
                     _textimc.setVisibility(view.VISIBLE);
+
                 }else {
                     Toast.makeText(MainActivity.this, "RG sem nome! OMG!!!", Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
+
+    public String[] checkImc(Double imc){
+            String peso = "";
+            String corTela ="";
+        if(imc <16){
+             peso = "Baixo peso muito grave";
+             corTela = "#1c1363";
+        }
+        else if(imc >= 16 && imc <= 16.99){
+            peso = "Baixo peso grave";
+            corTela = "#0156a6";
+        }
+        else if(imc >= 17 && imc <= 18.49){
+            peso = "Baixo peso ";
+            corTela = "#00b0f7";
+        }
+        else if(imc >= 18.50 && imc <= 24.99){
+            peso = "Peso normal";
+            corTela = "#00a64c";
+        }
+        else if(imc >= 25 && imc <= 29.99){
+            peso = "Sobrepeso";
+            corTela = "#fae501";
+        }
+        else if(imc >= 30.50 && imc <= 34.99){
+            peso = "Obesidade grau I";
+            corTela = "#ff9a03";
+        }
+        else if(imc >= 35 && imc <= 39.99){
+            peso = "Obesidade grau II";
+            corTela = "#cf4702";
+        }
+        else if(imc >= 40){
+            peso = "Obesidade grau III (obesidade mórbida)";
+            corTela = "#a00001";
+        }
+        return new String[]{peso, corTela};
+    }
 }
+
